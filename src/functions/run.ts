1d956402e4aadc76e14a1d912597efef3021aa67
Chris Tate <chris@austincodeshop.com>
2019-06-14 03:41:46 -0500

    adds validation

--- src/functions/run.ts
+++ src/functions/run.ts
@@ -1,23 +1,49 @@
-import { IProgram } from '../interfaces';
+import chalk from 'chalk';
+
+import { IArg, IProgram } from '../interfaces';
 import { parseArgv } from '../functions';
 
 export const run = (program: IProgram) => {
-  const { action, args, props } = parseArgv(process.argv.slice(2));
+  try {
+    const { action, args, props } = parseArgv(process.argv.slice(2));
 
-  if (!action) {
-    console.log('Usage: mvs <action>')
-    return;
-  }
+    if (!action) {
+      console.log('Usage: mvs <action>')
+      return;
+    }
 
-  const p = program({ args });
+    const p = program({ args });
 
-  if (props.help) {
-    const allArgs = p.actions[action].args ? `${p.actions[action].args.map(a => ` <${a.enum ? a.enum.join('|') : a.name}>`).join('')}` : '';
-    console.log(`Usage: mvs ${action}${allArgs}`);
-    return;
-  }
+    if (props.help) {
+      const parseArg = (arg: IArg) => {
+        let output = ' ';
+        output += arg.required ? '<' : '[';
+        if (arg.enum) {
+          output += arg.enum.join('|');
+        } else {
+          output += arg.name;
+        }
+        output += arg.required ? '>' : ']';
+        return output;
+      };
+      const allArgs = p.actions[action].args ? p.actions[action].args.map(parseArg).join('') : '';
+      console.log(`Usage: mvs ${action}${allArgs}`);
+      return;
+    }
 
-  if (p.actions[action]) {
-    p.actions[action].action(props);
+    if (p.actions[action]) {
+      if (p.actions[action].args) {
+        p.actions[action].args.forEach((arg, index) => {
+          if (arg.required && !args[index]) {
+            throw new Error(`<${arg.name}> is required`);
+          } else if (arg.enum && !arg.enum.includes(args[index])) {
+            throw new Error(`<${arg.name}> must be one of the following: ${arg.enum.join(', ')}`);
+          }
+        });
+      }
+      p.actions[action].action(props);
+    }
+  } catch (error) {
+    console.log(chalk.red(`ERROR: ${error.message}`));
   }
 };
