0d6d8359e1214a9b06c21a9feae1223f07a77a6e
Chris Tate <chris@austincodeshop.com>
2019-06-24 00:14:13 -0500

    hanles multiple programs

--- src/functions/run.ts
+++ src/functions/run.ts
@@ -3,16 +3,21 @@ import chalk from 'chalk';
 import { IArg, IProgram } from '../interfaces';
 import { parseArgv } from '../functions';
 
-export const run = (program: IProgram) => {
+export const run = async (program: IProgram) => {
   try {
+    const programName = process.argv[1].split('/').slice(-1)[0];
     const { action, args, props } = parseArgv(process.argv.slice(2));
 
+    const p = program({ args });
+
     if (!action) {
-      console.log('Usage: mvs <action>')
+      console.log(`Usage:\n\n${programName} <action>\n`)
+      console.log('Actions:\n');
+      Object.keys(p.actions).forEach((actionName) => {
+        console.log(`- ${actionName}`);
+      });
       return;
     }
-
-    const p = program({ args });
 
     if (props.help) {
       const parseArg = (arg: IArg) => {
@@ -27,21 +32,21 @@ export const run = (program: IProgram) => {
         return output;
       };
       const allArgs = p.actions[action].args ? p.actions[action].args.map(parseArg).join('') : '';
-      console.log(`Usage: mvs ${action}${allArgs}`);
+      console.log(`Usage: ${programName} ${action}${allArgs}`);
       return;
     }
 
     if (p.actions[action]) {
       if (p.actions[action].args) {
         p.actions[action].args.forEach((arg, index) => {
-          if (arg.required && !args[index]) {
-            throw new Error(`<${arg.name}> is required`);
-          } else if (arg.enum && args[index] && !arg.enum.includes(args[index])) {
+          if (arg.enum && (arg.required || args[index]) && !arg.enum.includes(args[index])) {
             throw new Error(`<${arg.name}> must be one of the following: ${arg.enum.join(', ')}`);
+          } else if (arg.required && !args[index]) {
+            throw new Error(`<${arg.name}> is required`);
           }
         });
       }
-      p.actions[action].action(props);
+      await p.actions[action].action(props);
     }
   } catch (error) {
     console.log(chalk.red(`ERROR: ${error.message}`));
--- src/interfaces/arg.ts
+++ src/interfaces/arg.ts
@@ -1,5 +1,5 @@
 export interface IArg {
   enum?: string[];
   name?: string;
-  required?: true;
+  required?: boolean;
 }
